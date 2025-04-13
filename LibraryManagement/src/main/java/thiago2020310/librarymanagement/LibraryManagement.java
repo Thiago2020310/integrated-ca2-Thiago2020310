459cefa6bd5943723a931c94aeeaf2b1ee02fbe7
Thiago2020310 <2020310@student.cct.ie>
2025-04-13 16:41:31 +0100

    Abstract User Class created
    User class will have the following protected attributes:
     ID: to be incremented for each user
     Name and email
     Password: In case authentication system is put in place
     Role: this will define if the user is student or admin, a function will be given to change the menu displayed on the console based on the role, permissions also will be different

--- LibraryManagement/src/main/java/thiago2020310/librarymanagement/LibraryManagement.java
+++ LibraryManagement/src/main/java/thiago2020310/librarymanagement/LibraryManagement.java
@@ -1,6 +1,6 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
+*/
 
 package thiago2020310.librarymanagement;
 
--- LibraryManagement/src/main/java/thiago2020310/librarymanagement/User.java
+++ LibraryManagement/src/main/java/thiago2020310/librarymanagement/User.java
@@ -1,13 +1,18 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+
  */
 package thiago2020310.librarymanagement;
 
 /**
  *
- * @author Thiago
+ * @author 
  */
 public abstract class User {
+    
+    protected int id;
+    protected String name;
+    protected String email;
+    protected String password;
+    protected String role;
     
 }
