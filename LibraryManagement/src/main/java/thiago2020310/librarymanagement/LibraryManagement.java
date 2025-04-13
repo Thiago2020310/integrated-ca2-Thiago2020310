53ed5cae6e41703fc512095ac2e5011d539d4553
Thiago2020310 <2020310@student.cct.ie>
2025-04-13 16:24:39 +0100

    Firt Project Commit
    Added java files to the project

---
+++ LibraryManagement/src/main/java/thiago2020310/librarymanagement/LibraryManagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package thiago2020310.librarymanagement;
+
+/**
+ *
+ * @author Thiago
+ */
+public class LibraryManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ LibraryManagement/src/main/java/thiago2020310/librarymanagement/User.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package thiago2020310.librarymanagement;
+
+/**
+ *
+ * @author Thiago
+ */
+public abstract class User {
+    
+}
